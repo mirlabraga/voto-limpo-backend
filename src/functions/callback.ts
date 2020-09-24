0fddc1f925011205223ace606b77a26fd980bec9
Mirla Chucre <mirlabraga@gmail.com>
2020-09-24 11:16:30 +0100

    Add function lambda for callback google oauth2

--- src/functions/callback.ts
+++ src/functions/callback.ts
@@ -1,12 +1,21 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
+import { APIGatewayProxyEvent, APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
+import { validateCode } from '../lib/oauth2';
 
-export const handler: APIGatewayProxyHandler = async (event, _context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
-  };
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent, _context) => {
+  const {code, state} = event.queryStringParameters;
+  try {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(await validateCode(code, state), null, 2),
+    };
+  } catch(e) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'something happens',
+        input: event,
+      }, null, 2),
+    };
+  }
 }
--- src/lib/oauth2.ts
+++ src/lib/oauth2.ts
@@ -1,7 +1,10 @@
+import fetch from 'node-fetch';
+import { URLSearchParams } from 'url';
 import { v4 as uuid } from 'uuid';
 
 const config = {
   signinUri: 'https://accounts.google.com/o/oauth2/v2/auth',
+  tokenUri: 'https://oauth2.googleapis.com/token',
   scope: 'openid profile email phone address https://www.googleapis.com/auth/contacts.readonly https://www.googleapis.com/auth/calendar',
   responseType: 'code',
   clientId: process.env.GOOGLE_CLIENT_ID,
@@ -11,6 +14,37 @@ const config = {
   callbackUrlBase: process.env.BASE_URL
 }
 
-export const buildRedictURI = () => {
+export interface OAuth2Token {
+  access_token: string,
+  expires_in: number,
+  scope?: string,
+  token_type: string,
+  id_token?: string
+}
+
+export const buildRedictURI = (): string => {
   return `${config.signinUri}?client_id=${config.clientId}&redirect_uri=${config.callbackUrlBase}/callback&scope=${config.scope}&response_type=${config.responseType}&state=${uuid()}&access_type=${config.accessType}&prompt=${config.prompt}`;
+}
+
+export const validateCode = async (code: string, _state: string): Promise<OAuth2Token> => {
+  const params = new URLSearchParams();
+  params.append('grant_type', 'authorization_code');
+  params.append('client_id', config.clientId);
+  params.append('client_secret', config.clientSecret);
+  params.append('redirect_uri', `${config.callbackUrlBase}/callback`);
+  params.append('code', code);
+
+  const response = await fetch(config.tokenUri, {
+    method: 'post',
+    body: params.toString(),
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  });
+
+  if (!response.ok) {
+    console.error(`Couldn't Exchange code. StatusCode: ${response.status}. Body: ${await response.text()}`);
+    throw new Error(`Couldn't Exchange code. StatusCode: ${response.status}`);
+  }
+  return await response.json() as OAuth2Token;
 }
