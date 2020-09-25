ac963fc2c9dd1661af18f2cbdd20d7672e98cdb9
Fernando Chucre <fernando@oraoncology.com>
2020-09-25 10:42:30 +0100

    fix handler types

--- src/lib/utilErrorHandlers.ts
+++ src/lib/utilErrorHandlers.ts
@@ -10,16 +10,12 @@ export class HttpError extends Error {
 type FuncType<T> = (event: APIGatewayProxyEventV2, context: Context) => Promise<T>;
 
 export const handlerErrors = <T>(func: FuncType<T>): APIGatewayProxyHandlerV2 => {
-
-  return (event: APIGatewayProxyEventV2, context: Context) => {
+  return async (event: APIGatewayProxyEventV2, context: Context): Promise<APIGatewayProxyStructuredResultV2> => {
     try {
-      const promise = func(event, context);
-      promise.then((result: T) => {
         return {
           statusCode: 200,
-          body: JSON.stringify(result)
+          body: JSON.stringify(await func(event, context))
         }
-      })
     } catch(e) {
       return {
         statusCode: e.statusCode || 500,
